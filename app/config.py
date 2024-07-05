318aa0fdf06f3a2b4b29e81df542b98cc1d1ab7d
caleb kibe <kiplagatcaleb2@gmail.com>
2024-07-05 19:05:37 +0300

    configures the app

--- app/config.py
+++ app/config.py
@@ -2,11 +2,6 @@ import os
 
 
 class Config:
-    # MySQL database connection URI
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://medbooker_dev:medbooker_dev_pwd@localhost/medbooker_dev_db'
-
-    # Disable SQLAlchemy modification tracking
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # Secret key for JWT
-    JWT_SECRET_KEY = os.urandom(24)
+    JWT_SECRET_KEY = os.urandom(32).hex()
