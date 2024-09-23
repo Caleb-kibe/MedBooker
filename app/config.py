364716ba4912e95838261af5f506229a43c12ea7
caleb kibe <kiplagatcaleb2@gmail.com>
2024-09-23 19:15:10 +0300

    configurations

--- app/config.py
+++ app/config.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://medbooker_dev:medbooker_dev_pwd@localhost/medbooker_dev_db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI_0')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.urandom(32).hex()
