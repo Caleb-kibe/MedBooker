6f09189568d5b22974c74a97dd5a3a52fb70b1dd
caleb kibe <kiplagatcaleb2@gmail.com>
2024-09-23 19:08:17 +0300

    creates my app

---
+++ app/create.py
@@ -0,0 +1,44 @@
+from flask import Flask
+import logging
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+from .config import Config
+from .models import db
+from flask_cors import CORS
+
+
+bcrypt = Bcrypt()
+jwt = JWTManager()
+logging.basicConfig(level=logging.INFO)
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    CORS(app)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+    migrate = Migrate(app, db)
+
+    with app.app_context():
+        # Import parts of our application
+        from . import routes, auth, models
+
+        # Register Blueprints
+        from .auth import auth_bp
+        from .routes import appointments_bp
+        from .routes import api_bp
+
+        app.register_blueprint(auth_bp, url_prefix='/auth')
+        app.register_blueprint(appointments_bp, url_prefix='/appointments')
+        app.register_blueprint(api_bp, url_prefix='/api')
+
+        # Create the database tables if they don't exist
+        db.create_all()
+
+    logging.info("Flask app started successfully")
+
+    return app
