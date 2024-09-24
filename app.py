721c741e6fc3c25bc9c6751ce86c2e711ff25f4f
caleb kibe <kiplagatcaleb2@gmail.com>
2024-09-24 14:53:35 +0300

    added home route

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from app.create import create_app
 
 app = create_app()
 
+@app.route('/')
+def home():
+    return "Welcome to MedBooker!"
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
