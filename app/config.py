e59685d2ff581e2693df059c88aeaccadc63213f
caleb kibe <kiplagatcaleb2@gmail.com>
2024-09-25 14:25:45 +0300

    set database configuration

--- app/config.py
+++ app/config.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI_1')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI_0')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.urandom(32).hex()
